39b839b141823364fbcb96920be021588a5eff15
Michael Lan <michael.lan20@imperial.ac.uk>
2021-02-10 14:23:05 +0000

    Implemented UDP server functionality

--- given templates/udp/UDPServer.java
+++ given templates/udp/UDPServer.java
@@ -4,10 +4,7 @@
 package udp;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
+import java.net.*;
 import java.util.Arrays;
 
 import common.MessageInfo;
@@ -26,6 +23,21 @@ public class UDPServer {
 
 		// TO-DO: Receive the messages and process them by calling processMessage(...).
 		//        Use a timeout (e.g. 30 secs) to ensure the program doesn't block forever
+		try {
+			pacSize = 1024;
+
+			while (true) {
+				pacData = new byte[pacSize];
+				pac = new DatagramPacket(pacData, pacSize);
+				recvSoc.setSoTimeout(30000);
+				recvSoc.receive(pac);
+				processMessage(new String(pac.getData()));
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -33,21 +45,48 @@ public class UDPServer {
 
 		MessageInfo msg = null;
 
-		// TO-DO: Use the data to construct a new MessageInfo object
+		// Use the data to construct a new MessageInfo object
+		try {
+			msg = new MessageInfo(data.trim());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		// TO-DO: On receipt of first message, initialise the receive buffer
+		// On receipt of first message, initialise the receive buffer
+		if (msg.messageNum == 0) {
+			totalMessages = msg.totalMessages;
+			receivedMessages = new int[totalMessages];
+			System.out.println("Receive buffer initialised");
+		}
 
 		// TO-DO: Log receipt of the message
+		receivedMessages[msg.messageNum] = 1;
+		System.out.println("Message " + (msg.messageNum + 1) + " received");
 
 		// TO-DO: If this is the last expected message, then identify
 		//        any missing messages
-
+		if (msg.messageNum == totalMessages - 1) {
+			System.out.println("Last expected message received");
+			int missingMessages = 0;
+			for (int i = 0; i < totalMessages; i++) {
+				if (receivedMessages[i] != 1) {
+					System.out.println("Message " + (i + 1) + " was lost");
+					missingMessages++;
+				}
+			}
+			System.out.println(missingMessages + " messages were lost in total");
+			System.out.println(msg.totalMessages + " were sent by the client");
+		}
 	}
 
 
 	public UDPServer(int rp) {
 		// TO-DO: Initialise UDP socket for receiving data
-
+		try {
+			recvSoc = new DatagramSocket(rp);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 		// Done Initialisation
 		System.out.println("UDPServer ready");
 	}
@@ -63,6 +102,14 @@ public class UDPServer {
 		recvPort = Integer.parseInt(args[0]);
 
 		// TO-DO: Construct Server object and start it by calling run().
-	}
+		UDPServer server = new UDPServer(recvPort);
+		try {
+			System.out.println("UDP server listening on: ");
+			System.out.println(InetAddress.getLocalHost().getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 
+		server.run();
+	}
 }
